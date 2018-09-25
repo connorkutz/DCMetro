55c87a961f695d438af54454ee05eb8e8cd34ded
connorkutz <connorkutz@gwu.edu>
2018-09-25 10:55:37 -0400

    project created

---
+++ app/src/main/java/kutz/connor/metroid/HomeScreenActivity.kt
@@ -0,0 +1,12 @@
+package kutz.connor.metroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+    }
+}
